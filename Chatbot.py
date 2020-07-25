004e9ef32d0d53ed44e44df85153a7bb95b04e3a
shahmanan13 <manan.vinod.shah@gmail.com>
2020-07-25 16:02:33 +0530

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,12 +3,23 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy
-import tensorflow
 import tflearn
+import tensorflow
 import random
 import json
 
-with open("intense.json") as file:
+with open("intents.json") as file:
 	data = json.load(file)
 
-print(data)
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+	for pattern in intent["patterns"]:
+		wrds = nltk.word_tokenize(pattern)
+		words.extend(wrds)
+		docs.append(pattern)
+
+	if intent["tag"] not in labels:
+		labels.append(intent["tag"])
