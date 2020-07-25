c18ceb7e76bfa9830573ca2c55f5067ab078f227
shahmanan13 <manan.vinod.shah@gmail.com>
2020-07-25 14:10:47 +0530

    Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,1 +1,14 @@
-In process!
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tensorflow
+import tflearn
+import random
+import json
+
+with open("intense.json") as file:
+	data = json.load(file)
+
+print(data)
