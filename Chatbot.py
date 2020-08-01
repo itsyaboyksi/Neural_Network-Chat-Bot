74a547df6a9c679223ba2c82f4fde706e5023a6d
Manan Shah <manan.vinod.shah@gmail.com>
2020-08-01 20:48:46 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,10 +2,10 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy as np
+import numpy
 import tflearn
-import tensorflow as tf
-import random
+import tensorflow
+import random	
 import json
 import pickle
 
@@ -21,8 +21,6 @@ except:
 	docs_x = []
 	docs_y = []
 
-	# nltk.download('punkt')
-
 	for intent in data["intents"]:
 		for pattern in intent["patterns"]:
 			wrds = nltk.word_tokenize(pattern)
@@ -33,7 +31,7 @@ except:
 		if intent["tag"] not in labels:
 			labels.append(intent["tag"])
 
-	words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+	words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 	words = sorted(list(set(words)))
 
 	labels = sorted(labels)
@@ -60,33 +58,26 @@ except:
 		training.append(bag)
 		output.append(output_row)
 
-	training = np.array(training)
-	output = np.array(output)
+
+	training = numpy.array(training)
+	output = numpy.array(output)
 
 	with open("data.pickle", "wb") as file:
 		pickle.dump((words, labels, training, output), file )
 
-tf.reset_default_graph()
+tensorflow.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation = 'softmax')
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
 
-try:
- 	model.load("model.tflearn")
-except:
-	model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
-	model.save("model.tflearn")
-
-'''
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-'''
+model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
+model.save("model.tflearn")
 
 def bag_of_words(s, words):
 	bag = [0 for _ in range(len(words))] 
@@ -99,22 +90,26 @@ def bag_of_words(s, words):
 			if w == se:
 				bag[i] = 1
 
-	return np.array(bag)
+	return numpy.array(bag)
 
 def chat():
-
 	print("Start Talking with Bot!(type quit to exit)")
 	while True:
 		inp = input("You: ")
 		if inp.lower() == "quit":
 			break
-		results = model.predict([bag_of_words(inp.lower(), words)])
-		results_index = np.argmax(results)
+		
+		results = model.predict([bag_of_words(inp.lower(), words)])[0]
+		results_index = numpy.argmax(results)
 		tag = labels[results_index]
 		
-		for tg in data["intents"]:
-			if tg['tag'] == tag:
-				responses = tg['responses']
+		if results[results_index] > 0.7:
+			for tg in data["intents"]:
+				if tg['tag'] == tag:
+					responses = tg['responses']
 
-		print(random.choice(responses))
+			print(random.choice(responses))
+		else:
+		 	print("I didn't get that, try again.")
+		
 chat()
